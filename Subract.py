45f00008885646752654305a13261104cb109059
Gayathri-29011998 <gayathrigs2998@gmail.com>
2025-01-23 14:59:08 +0530

    Add files via upload

---
+++ Subract.py
@@ -0,0 +1,10 @@
+# This program adds two numbers
+
+num1 = 5
+num2 = 3
+
+# Add two numbers
+float = num1 - num2
+
+# Display the sum
+print('The float of {0} and {1} is {2}'.format(num1, num2, float))
