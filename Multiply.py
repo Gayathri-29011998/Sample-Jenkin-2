6c93a0124812f978d386ed963974640a7a9be0a9
Gayathri-29011998 <gayathrigs2998@gmail.com>
2025-01-23 15:08:00 +0530

    Add files via upload

---
+++ Multiply.py
@@ -0,0 +1,10 @@
+# This program multiply two numbers
+
+num1 = 5
+num2 = 3
+
+# Multiply two numbers
+float = num1 * num2
+
+# Display the Multiply
+print('The float of {0} and {1} is {2}'.format(num1, num2, float))
